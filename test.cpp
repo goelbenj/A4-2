bd5ae8950580d2f7df9d55ea99c3d755c8421d44
goelbenj <ben_goel@rogers.com>
2023-11-22 17:34:04 -0500

    Add time measurement

--- test.cpp
+++ test.cpp
@@ -1,10 +1,12 @@
+#include <chrono>         // std::chrono
+#include <iomanip>
 #include <iostream>       // std::cout
 #include <thread>         // std::thread
 #include <mutex>          // std::mutex
 #include "linked_list.h"
 
 
-void coarse_vs_fine_test(void (*insertion_func)(List *, int), int num_insertions, int *values) {
+double coarse_vs_fine_test(void (*insertion_func)(List *, int), int num_insertions, int *values) {
     List* list = new List();
     insertion_func(list, 40);
     insertion_func(list, 50);
@@ -15,21 +17,31 @@ void coarse_vs_fine_test(void (*insertion_func)(List *, int), int num_insertions
 
     // perform simultaneous insertions using global lock
     std::thread threads[num_insertions];
+    // start timer
+    auto start = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < num_insertions; i++) {
         threads[i] = std::thread(insertion_func, list, values[i]);
     }
 
     for (auto& th : threads) th.join();
+    auto stop = std::chrono::high_resolution_clock::now();
     print_list(list);
+    double diff = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count();
+    diff *= 1e-9;
+    return diff;
 }
 
 int main(int argc, char* argv[]) {
     // perform simultaneous insertions using global lock
     constexpr int num_insertions = 2;
     int values[num_insertions] = {65, 77};
-    coarse_vs_fine_test(insert_glock_sort, num_insertions, values);
+    double duration = coarse_vs_fine_test(insert_glock_sort, num_insertions, values);
+    std::cout << "Total duration: " << std::setw(9)
+                  << duration << " seconds\n";
 
     // perform simultaneous insertions using fine-grained lock
-    coarse_vs_fine_test(insert_flock_sort, num_insertions, values);
+    duration = coarse_vs_fine_test(insert_flock_sort, num_insertions, values);
+    std::cout << "Total duration: " << std::setw(9)
+                  << duration << " seconds\n";
     return 0;
 }
