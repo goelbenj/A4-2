400c6c39e176e0c380927d3d70a7f63527ae022d
goelbenj <ben_goel@rogers.com>
2023-11-22 17:10:40 -0500

    Refactor test driver

--- test.cpp
+++ test.cpp
@@ -4,40 +4,32 @@
 #include "linked_list.h"
 
 
-int main(int argc, char* argv[]) {
+void coarse_vs_fine_test(void (*insertion_func)(List *, int), int num_insertions, int *values) {
     List* list = new List();
-    insert_glock_sort(list, 40);
-    insert_glock_sort(list, 50);
-    insert_glock_sort(list, 100);
-    insert_glock_sort(list, 120);
-    insert_glock_sort(list, 160);
-    insert_glock_sort(list, 180);
+    insertion_func(list, 40);
+    insertion_func(list, 50);
+    insertion_func(list, 100);
+    insertion_func(list, 120);
+    insertion_func(list, 160);
+    insertion_func(list, 180);
 
     // perform simultaneous insertions using global lock
-    constexpr int num_insertions = 2;
     std::thread threads[num_insertions];
-    int values[num_insertions] = {65, 77};
     for (int i = 0; i < num_insertions; i++) {
-        threads[i] = std::thread(insert_glock_sort, list, values[i]);
+        threads[i] = std::thread(insertion_func, list, values[i]);
     }
 
     for (auto& th : threads) th.join();
     print_list(list);
+}
+
+int main(int argc, char* argv[]) {
+    // perform simultaneous insertions using global lock
+    constexpr int num_insertions = 2;
+    int values[num_insertions] = {65, 77};
+    coarse_vs_fine_test(insert_glock_sort, num_insertions, values);
 
     // perform simultaneous insertions using fine-grained lock
-    list = new List();
-    insert_flock_sort(list, 40);
-    insert_flock_sort(list, 50);
-    insert_flock_sort(list, 100);
-    insert_flock_sort(list, 120);
-    insert_flock_sort(list, 160);
-    insert_flock_sort(list, 180);
-    for (int i = 0; i < num_insertions; i++) {
-        threads[i] = std::thread(insert_flock_sort, list, values[i]);
-    }
-
-    for (auto& th : threads) th.join();
-    print_list(list);
-
+    coarse_vs_fine_test(insert_flock_sort, num_insertions, values);
     return 0;
 }
